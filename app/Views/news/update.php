b7f20041e789ac67a338a66107b0ed4b19a0a864
ariramadh <58457172+ariramadh@users.noreply.github.com>
2022-12-31 16:13:34 +0700

    Form Update
    A little cleaning

--- app/Views/news/update.php
+++ app/Views/news/update.php
@@ -1,5 +1,7 @@
-<label for="title">Title</label>
-<input name="title" type="text" value="<?= esc($news['title']) ?>">
+<p><label for="title">Title</label></p>
+<p><input name="title" type="text" value="<?= esc($news['title']) ?>">
 
-<label for="body">Text</label>
-<textarea name="body" type="text" cols="45" rows="4"><?= esc($news['body']) ?></textarea>
+<p><label for="body">Text</label></p>
+<p><textarea name="body" type="text" cols="45" rows="4"><?= esc($news['body']) ?></textarea></p>
+
+<input name="submit-update "type="button" value="Update data">
